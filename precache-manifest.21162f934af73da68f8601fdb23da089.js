0fd62bd60e6a5c4b848c0d20baa998a605dbb060
juan camilo acevedo <juancaacevedo10@hotmail.com>
2020-06-22 14:49:01 -0500

    Updates

---
+++ precache-manifest.21162f934af73da68f8601fdb23da089.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34fca9160254ca9676e1c70f7c2a5461",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "084c1fa305a690b3b344",
+    "url": "/portafolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "998b7f27e7dcaa6b0576",
+    "url": "/portafolio/static/css/main.c56273df.chunk.css"
+  },
+  {
+    "revision": "084c1fa305a690b3b344",
+    "url": "/portafolio/static/js/2.814a3c7d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portafolio/static/js/2.814a3c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998b7f27e7dcaa6b0576",
+    "url": "/portafolio/static/js/main.5fb328a9.chunk.js"
+  },
+  {
+    "revision": "d30650f1ec318e36fc57",
+    "url": "/portafolio/static/js/runtime-main.1d39578f.js"
+  }
+]);
